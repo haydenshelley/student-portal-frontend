82905c6fa99293f2699d71bd921f1f1601f00ca7
Hayden Shelley <hayden.m.shelley@gmail.com>
2023-09-26 11:46:06 -0700

    user authentication

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import { Header } from "./Header";
 import { Content } from "./Content";
 import { Footer } from "./Footer";
+import { Login } from "./Login";
+import { LogoutLink } from "./LogoutLink";
 
 function App() {
   return (
     <div>
       <Header />
       <Content />
+      <Login />
       <Footer />
     </div>
   );
