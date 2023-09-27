48058e465f65411ff8c96243471dc673bbd9ca73
ricky martinez <rickyjmartinez2@gmail.com>
2023-09-27 12:20:33 -0400

    update axios request to be dynamic

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,7 +1,23 @@
+import axios from "axios";
+import { useState, useEffect } from "react";
+import { StudentShow } from "./StudentShow";
+
 export function Content() {
+  const [studentInfo, setStudentInfo] = useState({});
+
+  const handleStudentShow = () => {
+    axios.get(`http://localhost:3000/students/${current_user.id}.json`).then((response) => {
+      console.log(response.data);
+      setStudentInfo(response.data);
+    });
+  };
+
+  useEffect(handleStudentShow, []);
+
   return (
     <div>
       <h1>Student Portal</h1>
+      <StudentShow studentInfo={studentInfo} />
     </div>
   );
 }
